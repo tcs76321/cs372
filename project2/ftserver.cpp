a1e302db6cfd4fa06beb9de07f0a71d6c4b65b36
Trevor Stahl <trevorclarkstahl@gmail.com>
2019-11-27 14:46:51 -0700

    Create ftserver.cpp

---
+++ project2/ftserver.cpp
@@ -0,0 +1,25 @@
+/*
+AUTHOR: Trevor Stahl, aka stahltr
+CLASS: CS 372 400 F2019
+PROGRAM: Project 2, ftp server client
+DESCRIPTION: 
+LAST MODIFIED(s): 
+	11/27/2019
+  */
+  
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
